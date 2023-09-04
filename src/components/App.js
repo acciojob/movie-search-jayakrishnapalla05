498152a096a9948146bfeab19be968201c5dcde4
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-09-04 21:57:27 +0530

    submitting code-Mon Sep 04 2023 21:57:26 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,44 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [search,setSearch]=useState("");
+  const [movieDetails,setMovieDetails]=useState([]);
+
+  async function getMovie(){
+    try{
+      let res=await fetch(`https://www.omdbapi.com/?s=${search}&page=1&apikey=91e8ca0b`)
+      let data=await res.json()
+      setMovieDetails(data.Search)
+      setSearch("");
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+  console.log(movieDetails);
+
+
+
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>Search Movie</div>
+        <form>
+          <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Search Movie"/>
+        </form>
+        <button onClick={getMovie}>Search</button>
+        <ul>
+          {movieDetails? movieDetails.map((movie,index)=>
+            <li key={index}>
+              <h2>{movie.Title} {`(${movie.Year})`}</h2>
+              <img src={movie.Poster}/>
+            </li>
+          ):<p className="error">Invalid movie name. Please try again.</p>}
+        </ul>
     </div>
   )
 }
